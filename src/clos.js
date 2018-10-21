69e09cc37a7c2ed5c10a1cdd34f51723d65b01f7
guenchi <louis.deguenchy@gmail.com>
2018-10-21 14:55:15 +0200

    Update clos.js

--- src/clos.js
+++ src/clos.js
@@ -1,1 +1,14 @@
 
+
+let defclass = (...o) => (...k) => (...v) => {
+    let x = {};
+    const len = k.length;
+    for(let i in o)
+    {
+        Object.assign(x, o[i]);
+    }
+    for(let i=0; i < len; i++) {
+        x[k[i]]=v[i];
+    }
+    return x;
+}
